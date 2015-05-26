397599cb1820565c1caaad26985f8dc03c8082ed
T-107 <T-107@PC266>
2015-05-25 19:52:24 -0500

    constructores y encapsulacion

---
+++ src/Constructores/Test1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Constructores;
+
+/**
+ *
+ * @author T-107
+ */
+public class Test1 {
+int x;
+String nombre;
+float sueldo;
+//se agrega un contructor y tiene una funcion q se relacionan los argumentos con los atributos
+//si agregas un constructor que no es parecido al constructor  por defecto entonces desaparece el de defecto
+public Test1(int valor){
+  
+}
+public Test1(){
+    //constructor por defecto
+}
+
+
+}
+
---
+++ src/Encapsulamiento/ProbarEncapsulamiento.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Encapsulamiento;
+
+/**
+ *
+ * @author T-107
+ */
+public class ProbarEncapsulamiento {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    //escribe java main class y te ahorras la fatiga
+        Usuario u1=new Usuario();
+       u1.setEdad(21);
+       System.out.println("Tu edad es: "+u1.getEdad());
+        
+    }
+    
+}
