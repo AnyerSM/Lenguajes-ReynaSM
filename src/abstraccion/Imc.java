6d35f941e7584752f1c650579b166f889f73cd33
T-107 <T-107@PC259>
2015-05-28 19:41:11 -0500

    clase 28 mayo

--- src/abstraccion/Imc.java
+++ src/abstraccion/Imc.java
@@ -10,16 +10,38 @@ package abstraccion;
  * @author T-107
  */
 public class Imc {
-  float peso;
-  float altura;
-  public Imc(float p, float a){
-  peso=p;
-  altura=a;
+  private float peso;
+  private float altura;
+  private boolean flaco;
+  public void setFlaco(boolean flaco){
+      this.flaco=flaco;
+        }
+    Imc(int i, float f) {
+    }
+    public boolean isFlaco(){ //en booleano get es "is" x convencion xq solo tienes dos valores
+        return flaco;  
+    }
   
+   public void setPeso(float peso) {
+        this.peso=peso;
+    }
+
+    public void setAltura(float altura)  {
+        this.altura=altura;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+    public Imc(){
+//constructor por defecto
   }
-  public String calcular(){
-  float imc=peso/(altura*altura);
-  return "tu imc es:"+imc;
-  
-   }
+
+    float calcular() {
+      return 0;
+    }
 }
