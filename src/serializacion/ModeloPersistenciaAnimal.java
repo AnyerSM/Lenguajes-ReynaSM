b2db37ed74ac518146f28c8255ccfeeea8e1f43c
T-107 <T-107@PC266>
2015-07-23 19:52:43 -0500

    arrays

--- src/serializacion/ModeloPersistenciaAnimal.java
+++ src/serializacion/ModeloPersistenciaAnimal.java
@@ -1,18 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package serializacion;
 
 import java.io.*;
+import java.util.*;
+
 public class ModeloPersistenciaAnimal {
-    public void guardar(Animal animal)throws Exception{
-        //paso 1 crear el archivo donde se guardara el am¿nimal
-        File file=new File("C:\\Users\\T101\\Documents\\animalitos.sss\\");//GenerasTusPropiasExtensionesYSoloLoPuedeLeerElPrograma
+    
+    String ruta="C:\\Users\\T101\\Documents\\animalitos.sss\\";
+    ArrayList<Animal> animales;
+    public ModeloPersistenciaAnimal(){
+        animales=new ArrayList<Animal>();
+    }
+    
+        public void guardar(Animal animal)throws Exception{
+        //paso 1 crear el archivo donde se guardara el animal
+        File file=new File(ruta);
+        if(file.exists()){
+            
+        }
+//GenerasTusPropiasExtensionesYSoloLoPuedeLeerElPrograma
     FileOutputStream fos=new FileOutputStream(file); //encadenamiento por constructor
     ObjectOutputStream oos=new ObjectOutputStream(fos);
-    oos.writeObject(animal);
+    animales.add(animal);
+    oos.writeObject(animales);
     System.out.println("Animalito comprimido con exito");
     }
-}
+public ArrayList<Animal> buscarTodos() throws Exception{
+    Animal a=new Animal();
+    File f=new File(ruta);
+    FileInputStream fis=new FileInputStream(f);
+    ObjectInputStream ois=new ObjectInputStream(fis);
+         animales= (ArrayList<Animal>)ois.readObject();
+         return animales;
+}}
